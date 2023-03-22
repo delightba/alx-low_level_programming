4e6caf1afc4cf771769e9310d1e0aff3298496c5
delightba <agboolasamuelbackup@gmail.com>
2023-03-22 11:59:03 +0100

    my phone

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet_x10 (void)
 {
-    print_alphabet_x10();
-    return (0);
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
 }
