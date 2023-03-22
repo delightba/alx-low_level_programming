d89f3c230341253c82e5ec7053956c965e5922dc
delightba <agboolasamuelbackup@gmail.com>
2023-03-22 11:37:13 +0100

    my phone

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-    int i;
+	int i;
 
-    for (i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	    _putchar(i);
-    _putchar('\n');
+	_putchar('\n');
 }
